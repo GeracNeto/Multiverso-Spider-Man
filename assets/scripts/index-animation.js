d167a66187c1a05832c713e4de17c5893b0e3492
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-05-21 21:13:20 -0300

    Atualizado nome do arquivo de script

--- assets/scripts/index-animation.js
+++ assets/scripts/index-animation.js
@@ -23,3 +23,18 @@ function addEventListenersToCards(){
 }
 
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+
+function selectCarouselItem(selectedButtonElement){
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector('.s-cards-carousel');
+    const transform = carousel.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem)-1);
+    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector('.s-controller__button--active');
+    activeButtonElement.classList.remove('s-controller__button--active');
+    selectedButtonElement.classList.add('s-controller__button--active');
+}
